118239819780463e227d8bf54c78a884e01272b0
Filidor Wiese <fili@fili.nl>
2015-12-08 21:19:51 +0100

    Removes sensitive data

---
+++ profiles/nvc-usd.profile.php
@@ -0,0 +1,21 @@
+<?php
+
+$CONFIG = array(
+	'btce_api_key' => '',
+	'btce_api_secret' => '',
+
+        'mysql_host' => 'localhost',
+        'mysql_user' => 'bitbot',
+        'mysql_password' => '',
+        'mysql_database' => 'bitbot',
+);
+
+$TRADE = array(
+	'trade_pair' => 'nvc_usd',
+	'trade_amount' => 0.2,
+	'trade_wait' => 8, 		// seconds
+	'trade_threshold' => 1,
+	'trade_stop_loss' => 95, 	// percentage
+	'trade_sma_short' => 10,	// minutes
+	'trade_sma_long' => 120,	// minutes
+);
---
+++ profiles/ticker.profile.php
@@ -0,0 +1,12 @@
+<?php
+
+$CONFIG = array(
+	'btce_api_key' => '',
+	'btce_api_secret' => '',
+        'btce_nonce_file' => '/tmp/nonce-ticker',
+
+        'mysql_host' => 'localhost',
+        'mysql_user' => 'bitbot',
+        'mysql_password' => '',
+        'mysql_database' => 'bitbot',
+);
